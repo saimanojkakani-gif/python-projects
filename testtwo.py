d0ae741fd9f2912824a11fb304acac5b0cbba08d
saimanojkakani-gif <saimanoj.kakani@gamil.com>
2026-06-25 20:54:10 +0530

    if else and elif

--- testtwo.py
+++ testtwo.py
@@ -5,3 +5,10 @@ if age >= 18:
     print("You are an adult")
 else:
     print("You are a minor")
+score = 60 
+if score >=90:
+    print("Grade A")
+elif score >=80:
+    print("Grade B")
+else:
+    print("Grade C")
